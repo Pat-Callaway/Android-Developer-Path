673e29bde94420458472a366d1486886ea23412a
Patrick Callaway <pcallaway0603@outlook.com>
2026-01-01 20:38:11 -0500

    Added correct imports, copied template to match tutorial

--- HappyBirthdayTwo/app/src/main/java/com/example/happybirthdaytwo/MainActivity.kt
+++ HappyBirthdayTwo/app/src/main/java/com/example/happybirthdaytwo/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,32 +18,27 @@ import com.example.happybirthdaytwo.ui.theme.HappyBirthdayTwoTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            HappyBirthdayTwoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            HappyBirthdayTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    HappyBirthdayTwoTheme {
-        Greeting("Android")
+fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
     }
+}
+
+@Composable
+fun HappyBirthdayTheme(content: @Composable () -> Unit) {
+    TODO("Not yet implemented")
 }
