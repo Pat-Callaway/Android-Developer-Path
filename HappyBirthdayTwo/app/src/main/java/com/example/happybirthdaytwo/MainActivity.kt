301cfe56ebfe4a3ce609eff8cce76abe8e989a81
Patrick Callaway <pcallaway0603@outlook.com>
2026-01-01 21:14:23 -0500

    Changed fontsize with .sp

--- HappyBirthdayTwo/app/src/main/java/com/example/happybirthdaytwo/MainActivity.kt
+++ HappyBirthdayTwo/app/src/main/java/com/example/happybirthdaytwo/MainActivity.kt
@@ -3,16 +3,14 @@ package com.example.happybirthdaytwo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.happybirthdaytwo.ui.theme.HappyBirthdayTwoTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,6 +33,7 @@ class MainActivity : ComponentActivity() {
 fun GreetingText(message: String, modifier: Modifier = Modifier){
     Text(
         text = message,
+        fontSize = 100.sp,
         modifier = modifier
     )
 }
